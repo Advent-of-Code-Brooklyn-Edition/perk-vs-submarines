04c781b202ba1eb3f0fb87b2d79199cc94b7b903
perkee <hello@perk.ee>
2021-12-03 15:55:12 -0500

    Update day03.js

--- day03.js
+++ day03.js
@@ -21,4 +21,34 @@ const gam = parseInt(
   , 2
 );
 
-console.log('part 1', ep * gam, 'part 2')
+const o2 = [11,10,9,8,7,6,5,4,3,2,1,0].reduce((nums, exp) => {
+  if(nums.length < 2 ) return nums; 
+  const mask = parseInt(
+    [11,10,9,8,7,6,5,4,3,2,1,0].map(
+      x => nums
+        .map(y => (y & (1 << x))/(1 << x))
+        .reduce((a, b) => a + b) >= nums.length / 2
+    ).map(x => x ? 0 : 1).join('')
+    , 2
+  );
+  return nums.filter(
+    x => (x & (1 << exp)) == (mask & (1 << exp)) 
+  )
+}, raw)[0];
+
+const co2 = [11,10,9,8,7,6,5,4,3,2,1,0].reduce((nums, exp) => {
+  if(nums.length < 2 ) return nums; 
+  const mask = parseInt(
+    [11,10,9,8,7,6,5,4,3,2,1,0].map(
+      x => nums
+        .map(y => (y & (1 << x))/(1 << x))
+        .reduce((a, b) => a + b) >= nums.length / 2
+    ).map(x => x ? 0 : 1).join('')
+    , 2
+  );
+  return nums.filter(
+    x => (x & (1 << exp)) == (mask & (1 << exp)) 
+  )
+}, raw)[0];
+
+console.log('part 1', ep * gam, 'part 2', o2 * co2)
