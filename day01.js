d6ff50c30d7c3551b2f95ddf223e0ac02e1846bf
perkee <hello@perk.ee>
2021-12-03 13:39:51 -0500

    added day1

--- day01.js
+++ day01.js
@@ -1,1 +1,19 @@
 // Day 1
+console.log(
+  'part 1',
+  document.querySelector('pre')
+    .innerText
+    .split('\n')
+    .map(Number)
+    .filter((v, i, a) => i && v > a[i - 1])
+    .length,
+  'part 2',
+  document.querySelector('pre')
+    .innerText
+    .split('\n')
+    .map(Number)
+    .map((v, i, a) => i > 1 && v + a[i - 1] + a[i - 2])
+    .slice(2)
+    .filter((v, i, a) => i && v > a[i - 1])
+    .length
+);
